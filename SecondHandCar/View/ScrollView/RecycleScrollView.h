11ca6056f22c5f1ff9d326df58e060cc7d9d1a3d
lifangli1234 <919186053@qq.com>
2015-08-27 18:02:30 +0800

    封装scrollView

---
+++ SecondHandCar/View/ScrollView/RecycleScrollView.h
@@ -0,0 +1,13 @@
+//
+//  RecycleScrollView.h
+//  SecondHandCar
+//
+//  Created by lifangli on 15/8/27.
+//  Copyright (c) 2015年 lifangli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecycleScrollView : UIView
+
+@end
---
+++ SecondHandCar/View/ScrollView/RecycleScrollView.m
@@ -0,0 +1,21 @@
+//
+//  RecycleScrollView.m
+//  SecondHandCar
+//
+//  Created by lifangli on 15/8/27.
+//  Copyright (c) 2015年 lifangli. All rights reserved.
+//
+
+#import "RecycleScrollView.h"
+
+@implementation RecycleScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
